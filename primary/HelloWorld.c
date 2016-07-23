e3624f25d9ae55fd1ad7b6f8dd5dbfec51c775fa
weihutaisui <380940813@qq.com>
2016-07-23 10:50:22 +0800

    20160723_02

--- primary/HelloWorld.c
+++ primary/HelloWorld.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 /*
-����
-ͨ�����������Ա���Ǵ�HelloWorld�𺽵ġ�
-Ҫ���ڿ���̨���Hello World��
+介绍
+通常大多数程序员都是从HelloWorld起航的。
+要求：在控制台输出Hello World！
 
-����
-ʹ��printf����ֱ�������
-ע�ⲻҪ�ô��ˣ�ͷ�ļ�Ҫ������main����Ҫ�У�mian�����ķ������Ϳ���ʹvoid��
+过程
+使用printf函数直接输出。
+注意不要敲错了！头文件要声明，main函数要有，mian函数的返回类型可以使void。
 
-ʱ�临�Ӷ�
+时间复杂度
 O(1)
-�ռ临�Ӷ�
+空间复杂度
 O(1)
 */
 
