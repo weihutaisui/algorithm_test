50adca609e4275384984f34d80a50f5f29ebc2c4
weihutaisui <380940813@qq.com>
2016-07-23 10:34:18 +0800

    20160723_01

---
+++ primary/HelloWorld.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/*
+介绍
+通常大多数程序员都是从HelloWorld起航的。
+要求：在控制台输出Hello World！
+
+过程
+使用printf函数直接输出。
+注意不要敲错了！头文件要声明，main函数要有，mian函数的返回类型可以使void。
+
+时间复杂度
+O(1)
+空间复杂度
+O(1)
+*/
+
+int main()
+{
+    printf("Hello  World!");
+    return 0;
+}
